b850ea6d9fa4d238ab38ce8111df81838bd956e6
Dmitry Andreyev <Krygliy.ya@gmail.com>
2023-11-06 18:34:07 +0700

    cartRedux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 import Search from './Search';
+import { useSelector } from 'react-redux';
 
 function Header() {
+   const { items, totalPrice } = useSelector((state) => state.cart);
+
    return (
       <div className="header">
          <div className="container">
@@ -17,7 +20,7 @@ function Header() {
             <Search />
             <div className="header__cart">
                <Link to="/cart" className="button button--cart">
-                  <span>520 ₽</span>
+                  <span>{totalPrice}</span>
                   <div className="button__delimiter"></div>
                   <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                      <path
@@ -42,7 +45,7 @@ function Header() {
                         strokeLinejoin="round"
                      />{' '}
                   </svg>
-                  <span>3</span>
+                  <span>{items.length}</span>
                </Link>
             </div>
          </div>
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -12,6 +12,7 @@ export const list = [
 ];
 
 function Sort() {
+   const sortRef = React.useRef();
    const dispatch = useDispatch();
    const sort = useSelector((state) => state.filter.sort);
    const onClickListItem = (obj) => {
@@ -20,9 +21,21 @@ function Sort() {
    };
 
    const [open, setOpen] = React.useState(false);
+   const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+         setOpen(false);
+      }
+   };
+
+   React.useEffect(() => {
+      document.body.addEventListener('click', handleClickOutside);
+      return () => {
+         document.body.removeEventListener('click', handleClickOutside);
+      };
+   }, []);
 
    return (
-      <div className="sort">
+      <div ref={sortRef} className="sort">
          <div className="sort__label">
             <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                <path
