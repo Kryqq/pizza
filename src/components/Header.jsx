2c8947631838ceb2f982f9a57ba4b2ee4e26b279
Dmitry Andreyev <Krygliy.ya@gmail.com>
2023-11-06 20:44:08 +0700

    cart/emptycart update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 
 function Header() {
    const { items, totalPrice } = useSelector((state) => state.cart);
+   const totalCount = items.reduce((sum, item) => sum + item.count, 0);
 
    return (
       <div className="header">
@@ -45,7 +46,7 @@ function Header() {
                         strokeLinejoin="round"
                      />{' '}
                   </svg>
-                  <span>{items.length}</span>
+                  <span>{totalCount}</span>
                </Link>
             </div>
          </div>
